875c3c62635c01c3c2059c53cdfcdf387cfc0c97
Harvey Quamen <hquamen@ualberta.ca>
2013-12-17 19:20:39 -0700

    Add Weather Data Transfer Object to repo

---
+++ WeatherDTO.php
@@ -0,0 +1,111 @@
+<?php
+
+class WeatherDTO {
+
+	/**********************************************************************
+	*	Huco 617.B2 (Winter 2014)
+	*	WeatherDTO is a data transfer object to facilitate moving weather
+	*	data around. Clients are free to work with their own data in 
+	*	any format they wish, but can get/set this DTO for easy access.
+	**********************************************************************/
+
+	protected $actualprecip;
+	protected $chanceprecip;
+	protected $date;
+	protected $hightemp;
+	protected $lowtemp;
+	protected $precipType;
+	protected $sitename;
+
+	
+	public function __construct() {
+		$this->date = new Date();
+	}
+	
+	/***************************
+	*	Site Name
+	***************************/
+	
+	public function getSiteName() {
+		return $this->sitename;
+	}
+	
+	public function setSiteName($sitename = null) {
+		$this->sitename = $sitename;
+	}
+	
+	/***************************
+	*	Date
+	***************************/
+	
+	public function getDate() {
+		return $this->date->getDateAsSQL();
+	}
+	
+	public function setDate($string) {
+		$this->date->setFromString($string);
+	}
+	
+	/***************************
+	*	High Temp
+	***************************/
+	
+	public function getHighTemp() {
+		return $this->hightemp;
+	}
+	
+	public function setHighTemp($temp = null) {
+		$this->hightemp = (int) $temp;
+	}
+	
+	/***************************
+	*	Low Temp
+	***************************/
+	
+	public function getLowTemp() {
+		return $this->lowtemp;
+	}
+	
+	public function setLowTemp($temp = null) {
+		$this->lowtemp = (int) $temp;
+	}
+	
+	/***************************
+	*	Chance of Precipitation
+	***************************/
+	
+	public function getChanceOfPrecip() {
+		return $this->chanceprecip;
+	}
+	
+	public function setChanceOfPrecip($precip = null) {
+		$this->chanceprecip = $precip;
+	}
+	
+	/***************************
+	*	Actual Precipitation
+	***************************/
+	
+	public function getActualPrecip() {
+		return $this->actualprecip;
+	}
+	
+	public function setActualPrecip($precip = null) {
+		$this->actualprecip = $precip;
+	}
+	
+	/***************************
+	*	Precipitation Type
+	***************************/
+	
+	public function getPrecipType() {
+		return $this->precipType;
+	}
+	
+	public function setPrecipType($type = null) {
+		$this->precipType = $type;
+	}
+
+}
+
+?>
