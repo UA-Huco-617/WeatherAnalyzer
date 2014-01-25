a53c02dd708a6ca2141ba240b66bb0565e4fa863
Harvey Quamen <hquamen@ualberta.ca>
2014-01-25 14:00:27 -0700

    Initial commit to repo

---
+++ RealWeatherDTO.php
@@ -0,0 +1,35 @@
+<?php
+
+class RealWeatherDTO extends WeatherDTO {
+
+	protected $actualprecip;
+	protected $cloudCover;
+	
+	/*****************************************
+	*	Actual Precipitation
+	*****************************************/
+	
+	public function getActualPrecip() {
+		return $this->actualprecip;
+	}
+	
+	public function setActualPrecip($precip = null) {
+		$this->actualprecip = $precip;
+	}
+
+	/*****************************************
+	*	Cloud Cover Percentage
+	*****************************************/
+	
+	public function getCloudCover() {
+		return $this->cloudCover;
+	}	
+
+	public function setCloudCover($percent = 0) {
+		$this->cloudCover = (int) $percent;
+	}
+
+
+}
+
+?>
