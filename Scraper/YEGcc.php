c736beb1b025375584ef1f0bdb2f78d0f4244661
Harvey Quamen <hquamen@ualberta.ca>
2014-01-25 14:37:23 -0700

    Add real weather scrapers to repo.

---
+++ Scraper/YEGcc.php
@@ -0,0 +1,10 @@
+<?php
+
+class Scraper_YEGcc extends Scraper_RealWeather {
+
+	protected $stationID = '31427';		//	Environment Canada's station #
+	protected $siteID = 2;				//	ID in weather_site database table
+
+}
+
+?>
---
+++ Scraper/YEGintl.php
@@ -0,0 +1,11 @@
+<?php
+
+class Scraper_YEGintl extends Scraper_RealWeather {
+
+	
+	protected $stationID = '50149';		//	Environment Canada's station #
+	protected $siteID = 1;				//	ID in weather_site database table
+
+}
+
+?>
