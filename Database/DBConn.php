d7468c943d22186f15610fbcf41d5ac0ed08c075
Harvey Quamen <hquamen@ualberta.ca>
2014-02-01 23:34:27 -0700

    Add singleton DB connection class to repo.

---
+++ Database/DBConn.php
@@ -0,0 +1,31 @@
+<?php
+
+//*****************************************************************
+//	Database Configuration
+//*****************************************************************
+
+define('HOST', '');
+define ('USER', '');
+define ('PWD', '');
+define ('DB', '');
+
+class Database_DBConn {
+
+	private static $connection;		//	the connection itself
+	
+	private function __construct() { }
+	
+	public static function getConnection() {
+		if ( empty( self::$connection ) ) {
+			try {
+				self::$connection =  new mysqli( HOST, USER, PWD, DB );
+			} catch ( Exception $e ) {
+				die( "I'm sorry. The database is temporarily down. Please try again later.\n" );
+			}
+		}
+		return self::$connection;
+	}
+
+}
+
+?>
