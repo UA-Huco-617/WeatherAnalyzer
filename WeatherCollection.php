f3f4b11283895e75ff919cae6cd5bee0c1273434
Harvey Quamen <hquamen@ualberta.ca>
2014-02-01 23:30:43 -0700

    Class implements Iterator and Countable interfaces

--- WeatherCollection.php
+++ WeatherCollection.php
@@ -1,13 +1,64 @@
 <?php
 
-class WeatherCollection implements Iterator {
+class WeatherCollection implements Iterator, Countable {
 
-//	we'll write this class as an in-class exercise
-
-
+	protected $collection = array();
+	protected $pointer = 0;				//	for Iterator
 
 	public function addToCollection($weatherdto) {
+		$this->collection[] = $weatherdto;
+	}
 	
+	public function saveToDatabase() {
+		$records_saved = 0;
+		foreach ($this->collection as $dto) {
+			if ($dto->saveToDatabase()) $records_saved++;
+		}
+		return $records_saved;
+	}
+
+	/*****************************************
+	*
+	*	Method to implement Countable
+	*
+	*****************************************/
+	
+	public function count() {
+		return count($this->collection);
+	}
+
+
+	/*****************************************
+	*
+	*	Methods to implement Iterator
+	*
+	*****************************************/
+
+	
+	public function rewind() {
+		$this->pointer = 0;
+	}
+	
+	public function current() {
+		return $this->collection[$this->pointer];
+	}
+	
+	public function key() {
+		return $this->pointer;
+	}
+	
+	public function next() {
+		$item = $this->collection[$this->pointer];
+		if ($item) $this->pointer++;
+		return $item;
+	}
+	
+	public function valid() {
+		if ($this->pointer >= count($this->collection) ) {
+			return false;
+		} else {
+			return (isset($this->collection[$this->pointer]));
+		}
 	}
 
 
