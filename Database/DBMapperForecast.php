3c6c8d3b133522bf41c1c0bab184859fdc42146e
Harvey Quamen <hquamen@ualberta.ca>
2014-02-01 23:25:13 -0700

    Adding working SQL queries to Database Mappers (will need to change if database schemas change)

--- Database/DBMapperForecast.php
+++ Database/DBMapperForecast.php
@@ -5,7 +5,10 @@ class Database_DBMapperForecast extends Database_DBMapper {
 	protected $table = 'weather_forecast';
 	
 	public function buildInsertQuery($clean) {
-		return "INSERT INTO {$this->table} VALUES (\N, ....";
+		return "INSERT INTO {$this->table} VALUES (\N, {$clean['site_id']}, {$clean['scrape_date']}, {$clean['forecast_date']}, 
+			{$clean['high']}, {$clean['high_unit']}, {$clean['low']}, {$clean['low_unit']}, {$clean['rain_amount']}, {$clean['rain_unit']},  
+			{$clean['snow_amount']}, {$clean['snow_unit']}, {$clean['chance_of_precip']}, {$clean['precip_amount']}, 
+			{$clean['precip_unit']}, {$clean['prose_forecast']})";
 	}
 	
 	public function getRawDTOData() {
@@ -13,6 +16,21 @@ class Database_DBMapperForecast extends Database_DBMapper {
 		$raw = array_fill_keys(array_keys($this->columnType), 0);
 		
 		//	collect data from $this->dto
+		$raw['site_id'] = $this->dto->getSiteID();
+		$raw['scrape_date'] = $this->dto->getTodayAsSQL();
+		$raw['forecast_date'] = $this->dto->getForecastDate();
+		$raw['high'] = $this->dto->getHighTemp(); 
+		$raw['high_unit'] = $this->dto->getTempUnit();
+		$raw['low'] = $this->dto->getLowTemp();
+		$raw['low_unit'] = $this->dto->getTempUnit();
+		$raw['rain_amount'] = $this->dto->getRainAmount();
+		$raw['rain_unit'] = $this->dto->getRainUnit();
+		$raw['snow_amount'] = $this->dto->getSnowAmount();
+		$raw['snow_unit'] = $this->dto->getSnowUnit();
+		$raw['chance_of_precip'] = $this->dto->getChanceOfPrecip();
+		$raw['precip_amount'] = $this->dto->getPrecipitation();
+		$raw['precip_unit'] = $this->dto->getPrecipitationUnit();
+		$raw['prose_forecast'] = $this->dto->getProseDescription();
 		return $raw;
 	}
 
--- Database/DBMapperReal.php
+++ Database/DBMapperReal.php
@@ -14,14 +14,15 @@ class Database_DBMapperReal extends Database_DBMapper {
 		//	make sure we have an array element for every column in the database table
 		$raw = array_fill_keys(array_keys($this->columnType), 0);
 		$raw['site_id'] = $this->dto->getSiteID();
-		$raw['date'] = $this->dto->getDate();
+		$raw['date'] = $this->dto->getForecastDate();	// will be yesterday
 		$raw['high'] = $this->dto->getHighTemp(); 
 		$raw['high_unit'] = $this->dto->getTempUnit();
 		$raw['low'] = $this->dto->getLowTemp();
 		$raw['low_unit'] = $this->dto->getTempUnit();
-		$raw['precip'] = $this->dto->getActualPrecip();
-		$raw['precip_unit'] = $this->dto->getPrecipUnit();
+		$raw['precip'] = $this->dto->getPrecipitation();
+		$raw['precip_unit'] = $this->dto->getPrecipitationUnit();
 		$raw['cloud_cover'] = $this->dto->getCloudCover();
+		//$raw['prose_description'] = $this->dto->getProseDescription();
 		return $raw;
 	}
 
