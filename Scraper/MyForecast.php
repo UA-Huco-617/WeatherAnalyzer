8a2bff7d1e985588fd047e3f15536bba3c8b4a0b
Lenoraven <sapach@ualberta.ca>
2014-02-03 15:57:51 -0700

    Working with values

--- Scraper/MyForecast.php
+++ Scraper/MyForecast.php
@@ -2,6 +2,8 @@
 
 class Scraper_MyForecast extends WeatherScraper{
 
+//I changed my siteID and siteURL...all else is same as template.
+	
 	protected $siteID = 6;				
 	protected $siteURL = 'http://www.myforecast.com/bin/expanded_forecast_15day.m?city=54149&metric=true';
 	protected $date;
@@ -41,14 +43,23 @@ class Scraper_MyForecast extends WeatherScraper{
 	}
 	
 
-//this is the function to fill out - main function!?
-public function scrape()
+
+//this is the function to fill out I only change things below here now.
+	public function scrape()
+
+
+
+
+
+
+
+
+
 
 
 //1. Get Page
 
 $html = file_get_contents('http://www.myforecast.com/bin/expanded_forecast_15day.m?city=54149&metric=true');
-
 
 //2. Clean up \n
 
@@ -60,8 +71,24 @@ $row_regex = '/<tr (.*?)<\/tr>/';
 
 preg_match_all($row_regex, $html, $row_matches);
 
+
 $cell_regex = '/<td align="center" valign="middle" class="normal">(.*?)<\/td>/';
 
+/*
+************************
+* Data                 *
+* proseDescription [0] *
+* High Temp [1]        *
+* Low Temp [2]         *
+* Wind Speed/Dir [3]   *
+* Humidity [4]         *
+* Comfort Level [5]    *
+* UV Index [6]         *
+* Prob Precip [7]      *
+* 24 h precip total [8]*
+************************
+
+*/
 
 //this is an attempt to isolate the dates for each forcast...need to figure out how to match to data...
 
@@ -69,11 +96,41 @@ $date_regex = '/<td align="left" valign="middle" bgcolor="#3366CC" class="wt">(.
 
 preg_match_all($date_regex, $html, $date_matches);
 
+
 //ok, so I have the dates, now to find a way to match the data
 //lets try making each day a var
+/*
+*************************
+* DATES ($date_matches) *
+* TODAY/Day 1 = [0][0]  *
+* Day 2 = [0][1]        *
+* Day 3 = [0][2]        *
+* Day 4 = [0][3]        *
+* Day 5 = [0][4]        *
+* Day 6 = [0][5]        *
+* Day 7 = [0][6]        *
+* Day 8 = [0][7]        *
+* Day 9 = [0][8]        *
+* Day 10 = [0][9]       *
+* Day 11 = [0][10]      *
+* Day 12 = [0][11]      *
+* Day 13 = [0][12]      *
+* Day 14 = [0][13]      *
+* Day 15 = [0][14]      *
+*************************
 
+*/ 
 
-print_r($date_matches[1]);
+public function extractDate($html) {
+		//	the forecast days are <a> tags, so the </a> here will fail on non-forecast days
+		$date_regex = '/<td align="left" valign="middle" bgcolor="#3366CC" class="wt">(.*?)<\/td>/';
+		if (preg_match($date_regex, $html, $matches)) {
+			return $matches[1] . ' ' . $matches[2];
+		} else {
+			return null;
+		}
+	}
+
 
 //4. Get all table data
 
