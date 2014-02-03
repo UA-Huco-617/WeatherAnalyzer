2a0d701f5f55a9efd8c41c66ce1a3ef63b6d6fc4
Lenoraven <sapach@ualberta.ca>
2014-02-03 11:56:45 -0700

    Added basic outline
    Added scraper template into file

--- Scraper/MyForecast.php
+++ Scraper/MyForecast.php
@@ -2,9 +2,47 @@
 
 class Scraper_MyForecast extends WeatherScraper{
 
+	protected $siteID = 6;				
+	protected $siteURL = 'http://www.myforecast.com/bin/expanded_forecast_15day.m?city=54149&metric=true';
+	protected $date;
+	protected $pageHasData = true;	
+	
+	public function __construct() {
+		parent::__construct();
+		$this->date = new Date();
+	}
+
+	public function __construct() {
+		$this->weathercollection = new WeatherCollection();
+		date_default_timezone_set('America/Edmonton');
+	}
+	
+	public function getSiteID() {
+		return $this->siteID;
+	}
+	
+	public function getSiteURL() {
+		return $this->siteURL;
+	}
+
+	public function getWeatherDTOCollection() {
+		//return a loaded Weather Data Collection
+		return $this->weathercollection;
+	}
+	
+	//	you're free to log messages! We'll perhaps push them
+	//	to a webpage so everyone can read them.
+	public function log($message = null) {
+		if (!empty($message)) Logger::log($message);
+	}
+	
+	public function cleanup($text = '') {
+		return str_replace("\n", ' ', $text);
+	}
+	
 
 
-
+public function scrape()
 
 
 
