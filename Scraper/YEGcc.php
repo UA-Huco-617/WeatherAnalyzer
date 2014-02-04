7d35d8fc153d37750e507af6085dd66f10a9722e
Harvey Quamen <hquamen@ualberta.ca>
2014-02-04 16:09:30 -0700

    Adding comments to explain this scraper's purpose

--- Scraper/YEGcc.php
+++ Scraper/YEGcc.php
@@ -2,6 +2,9 @@
 
 class Scraper_YEGcc extends RealWeatherScraper {
 
+	//	This scraper pulls real, historical weather data
+	//	from the Edmonton City Centre airport.
+	
 	protected $stationID = '31427';		//	Environment Canada's station #
 	protected $siteID = 2;				//	ID in weather_site database table
 
--- Scraper/YEGintl.php
+++ Scraper/YEGintl.php
@@ -2,6 +2,8 @@
 
 class Scraper_YEGintl extends RealWeatherScraper {
 
+	//	This scraper pulls real, historical weather data
+	//	from the Edmonton International airport.
 	
 	protected $stationID = '50149';		//	Environment Canada's station #
 	protected $siteID = 1;				//	ID in weather_site database table
