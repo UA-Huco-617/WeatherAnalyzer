747f25f17ac1001eb049de5fbd93973ae2f4d1f3
Harvey Quamen <hquamen@ualberta.ca>
2014-02-07 19:49:20 -0700

    Adding 7 Feb version of the SQL dumpfile

--- SQL/weatherdump-2014-02-07.sql
+++ SQL/weatherdump-2014-02-07.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.11, for osx10.7 (x86_64)
+-- MySQL dump 10.13  Distrib 5.1.39, for portbld-freebsd8.0 (amd64)
 --
--- Host: localhost    Database: weather
+-- Host: localhost    Database: birdclub
 -- ------------------------------------------------------
--- Server version	5.6.11
+-- Server version	5.1.39-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,32 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `weather_site`
+--
+
+DROP TABLE IF EXISTS `weather_site`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `weather_site` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `site_name` varchar(200) DEFAULT NULL,
+  `site_url` varchar(250) DEFAULT NULL,
+  `notes` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `weather_site`
+--
+
+LOCK TABLES `weather_site` WRITE;
+/*!40000 ALTER TABLE `weather_site` DISABLE KEYS */;
+INSERT INTO `weather_site` VALUES (1,'Edmonton International Airport','http://climate.weather.gc.ca/climateData/dailydata_e.html?StationID=50149&Year=2014&Month=1','Note StationID; modify GET parameters for month'),(2,'Edmonton City Centre AWOS','http://climate.weather.gc.ca/climateData/dailydata_e.html?StationID=31427&Year=2014&Month=1','Sometimes misses data, but note StationID and modify GET parameters for month'),(3,'OpenWeather.com (6-day)','http://www.weatherforecastmap.com/canada/edmonton',NULL),(4,'Foreca (10-day)','http://www.foreca.com/Canada/Edmonton?tenday',NULL),(5,'CBC (6-day)','http://www.cbc.ca/edmonton/weather/s0000045.html',NULL),(6,'MyForecast (15-day)','http://www.myforecast.com/bin/expanded_forecast_15day.m?city=54149&metric=true',NULL),(7,'AccuWeather (45-day)','http://www.accuweather.com/en/ca/edmonton/t5k/daily-weather-forecast/52478','Uses \'precip\' instead of \'rain\''),(8,'Tory Building Weather Station','http://easweb.eas.ualberta.ca/weather_archive.php','Has only temps, pressure and humidity'),(9,'Weather-in-Canada.com (14-day)','http://www.weather-in-canada.com/Alberta/Weather_in_Edmonton/14-day-forecast','Wind direction uses \"V\" for \"W\" (I think)');
+/*!40000 ALTER TABLE `weather_site` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `weather_forecast`
@@ -38,6 +64,9 @@ CREATE TABLE `weather_forecast` (
   `chance_of_precip` tinyint(3) unsigned DEFAULT NULL,
   `precip_amount` decimal(4,1) DEFAULT NULL,
   `precip_unit` char(2) DEFAULT 'mm',
+  `wind_speed` tinyint(3) unsigned DEFAULT NULL,
+  `wind_unit` varchar(4) DEFAULT 'km/h',
+  `wind_direction` smallint(5) unsigned DEFAULT NULL,
   `prose_forecast` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -70,6 +99,9 @@ CREATE TABLE `weather_real` (
   `precip` decimal(4,1) DEFAULT NULL,
   `precip_unit` char(2) NOT NULL DEFAULT 'mm',
   `cloud_cover` tinyint(3) unsigned DEFAULT NULL,
+  `wind_speed` tinyint(3) unsigned DEFAULT NULL,
+  `wind_unit` varchar(4) DEFAULT 'km/h',
+  `wind_direction` smallint(5) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `site_id` (`site_id`,`date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -83,32 +115,6 @@ LOCK TABLES `weather_real` WRITE;
 /*!40000 ALTER TABLE `weather_real` DISABLE KEYS */;
 /*!40000 ALTER TABLE `weather_real` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `weather_site`
---
-
-DROP TABLE IF EXISTS `weather_site`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `weather_site` (
-  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `site_name` varchar(200) DEFAULT NULL,
-  `site_url` varchar(250) DEFAULT NULL,
-  `notes` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `weather_site`
---
-
-LOCK TABLES `weather_site` WRITE;
-/*!40000 ALTER TABLE `weather_site` DISABLE KEYS */;
-INSERT INTO `weather_site` VALUES (1,'Edmonton International Airport','http://climate.weather.gc.ca/climateData/dailydata_e.html?StationID=50149&Year=2014&Month=1','Note StationID; modify GET parameters for month'),(2,'Edmonton City Centre AWOS','http://climate.weather.gc.ca/climateData/dailydata_e.html?StationID=31427&Year=2014&Month=1','Sometimes misses data, but note StationID and modify GET parameters for month'),(3,'OpenWeather.com (6-day)','http://www.weatherforecastmap.com/canada/edmonton',NULL),(4,'Foreca (10-day)','http://www.foreca.com/Canada/Edmonton?tenday',NULL),(5,'CBC (6-day)','http://www.cbc.ca/edmonton/weather/s0000045.html',NULL),(6,'MyForecast (15-day)','http://www.myforecast.com/bin/expanded_forecast_15day.m?city=54149&metric=true',NULL),(7,'AccuWeather (45-day)','http://www.accuweather.com/en/ca/edmonton/t5k/daily-weather-forecast/52478','Uses \'precip\' instead of \'rain\'');
-/*!40000 ALTER TABLE `weather_site` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -119,4 +125,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-02-02 10:29:02
+-- Dump completed on 2014-02-07 19:43:36
