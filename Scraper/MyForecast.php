6d0861828c896b473b3520041f454838b4242c06
Lenoraven <sapach@ualberta.ca>
2014-02-08 11:01:01 -0700

    Cleaned up scrape function
    In the process of working through creation of scrape
    function...attempting to figure out date function.

--- Scraper/MyForecast.php
+++ Scraper/MyForecast.php
@@ -1,8 +1,6 @@
 <?php
 
 class Scraper_MyForecast extends WeatherScraper{
-
-//I changed my siteID and siteURL...all else is same as template.
 	
 	protected $siteID = 6;				
 	protected $siteURL = 'http://www.myforecast.com/bin/expanded_forecast_15day.m?city=54149&metric=true';
@@ -28,12 +26,10 @@ class Scraper_MyForecast extends WeatherScraper{
 	}
 
 	public function getWeatherDTOCollection() {
-		//return a loaded Weather Data Collection
+
 		return $this->weathercollection;
 	}
-	
-	//	you're free to log messages! We'll perhaps push them
-	//	to a webpage so everyone can read them.
+
 	public function log($message = null) {
 		if (!empty($message)) Logger::log($message);
 	}
@@ -42,28 +38,40 @@ class Scraper_MyForecast extends WeatherScraper{
 		return str_replace("\n", ' ', $text);
 	}
 	
+	
+	}
 
 
 //this is the function to fill out I only change things below here now.
-	public function scrape()
+	//THE FUNCTIONS ALREADY EXIST!  call getters and setters....WeatherDTO and DATE
+	//remember to do the top-down narrative thing...every function followed by next level of abstractions
 
+	public function scrape() {
+		$html = file_get_contents($this->$siteURL);
+		$html = $this->cleanup($html);
+		//can I make a function that will set each day as a separate row to then convert to dto?
+		//or do I need to do a separate command here for each day?
+	}
 
+//Day 1 - Today
 
+	public function extractTodaysRow($html) {
+		$today = new Date('Today');
+		
+		$date = $today->setYear(2014);
+		$date = $today->setMonthByName();
+		$date = $today->setDay();
 
+		$regex = '/<td align="left" valign="middle" bgcolor="#3366CC" class="wt">(.*?)<\/tr>/';
+		preg_match($regex, $html, $matches);
+		return $matches[1];
+	}
 
+	public function setDTOFromRow($row) {
+		$dto = new WeatherDTO($this);
+		$regex = 
 
-
-
-
-
-
-//1. Get Page
-
-$html = file_get_contents('http://www.myforecast.com/bin/expanded_forecast_15day.m?city=54149&metric=true');
-
-//2. Clean up \n
-
-$html = str_replace("\n", ' ', $html );
+	}
 
 //3. Get all rows
 
@@ -87,6 +95,27 @@ $cell_regex = '/<td align="center" valign="middle" class="normal">(.*?)<\/td>/';
 * Prob Precip [7]      *
 * 24 h precip total [8]*
 ************************
+
+
+/*
+These are the variables that Harvey has listed, so make the above match the exact terms - for my own ease of understanding!
+      protected $chanceprecip;
+      protected $date;                    //    forecast date
+      protected $hightemp;
+      protected $lowtemp;
+      protected $rainAmount;
+      protected $rainUnit = 'mm';         //    default value
+      protected $proseDescription;  //    i.e., 'partly cloudy'
+      protected $precipitation;
+      protected $precipUnit = 'mm'; // default unit
+      protected $snowAmount;
+      protected $snowUnit = 'cm';         //    default value
+      protected $tempUnit = 'C';          //    default value
+      
+      protected $scraper;                       //    scraper that built this object
+      protected $siteID;                        //    scraper's site ID
+      protected $dbhelper;                //    DatabaseMapper object to handle I/O
+*/
 
 */
 
@@ -121,16 +150,6 @@ preg_match_all($date_regex, $html, $date_matches);
 
 */ 
 
-public function extractDate($html) {
-		//	the forecast days are <a> tags, so the </a> here will fail on non-forecast days
-		$date_regex = '/<td align="left" valign="middle" bgcolor="#3366CC" class="wt">(.*?)<\/td>/';
-		if (preg_match($date_regex, $html, $matches)) {
-			return $matches[1] . ' ' . $matches[2];
-		} else {
-			return null;
-		}
-	}
-
 
 //4. Get all table data
 
@@ -148,23 +167,56 @@ foreach ( $row_matches[1] as $row) {
 
 
 
-// here down is other copied stuff to maybe put in/play with!
-//looks like this creates the DTO to be sent out.
-public function extractWeatherData($rows) {
-		foreach ($rows as $row) {
-			if ( $this->haveValidRow($row)) {
-				$dto = $this->buildDTO($row);
-				if ($dto) {
-					$date = $this->extractDate($row);
-					$dto->setForecastDate($date);
-					$this->weathercollection->addToCollection($dto);
-				}
-				//var_dump($dto);
-			}
+// here down is other copied stuff to maybe put in/play with! -----------------
+// keeping in mind that above I have already moved table data and dates into arrays - so if I can sort the above arrays, may not need some of these functions?
+//to create the dto to send out...this is the version from Harvey's AccuWeather45 - what are my differences?
+//see...high, low, etc. is ONLY designated here....not the str replace bit!  ah ha
+public function buildDTO($html) {
+		/******************************************************************
+		*	The regex produces this data:
+		*	[1][0] => high (remove entity: &#176;)
+		*	[1][1] => low (remove entity: &#176;)
+		*	[1][2] => precip (split to capture unit: prob mm)
+		*	[1][3] => snow (split to capture unit: prob cm)
+		*	[1][4] => forecast (remove image; keep prose)
+		*	[1][5] => avg hi (ignore)
+		*	[1][6] => avg low (ignore)
+		*	
+		*	The row has no data if we have </th>\s*<td>&nbsp;</td>
+		******************************************************************/
+		if (stripos($html, "\n") !== false ) $html = $this->cleanup($html);
+		if ( !$this->haveValidRow($html)) return null;
+		$dto = new WeatherDTO($this);
+		$regex = '/<td(?:.*?)>(.+?)<\/td>/';
+		preg_match_all($regex, $html, $m);
+		//	HIGH
+		$high = str_replace('&#176;', '', $m[1][0]);
+		$dto->setHighTemp($high);
+		//	LOW
+		$low = str_replace('&#176;', '', $m[1][1]);
+		$dto->setLowTemp($low);
+		//	PRECIP
+		//	echo "Precip line: {$m[1][2]}\n";
+		if (isset($m[1][2]) && preg_match('/\d/', $m[1][2])) {
+			list( $precip, $punit ) = explode(' ', $m[1][2]);
+			$dto->setPrecipitation($precip);
+			$dto->setPrecipitationUnit($punit);
 		}
+		//	SNOW -- this stays in the summer, but they
+		//	seem to use the Precip column for rain.
+		//	echo "Snow line: {$m[1][3]}\n";
+		if (isset($m[1][3]) && preg_match('/\d/', $m[1][3])) {
+			list($snow, $sunit) = explode(' ', $m[1][3]);
+			$dto->setSnowAmount($snow);
+			$dto->setSnowUnit($sunit);
+		}
+		//	FORECAST
+		if (isset($m[1][4]) && !preg_match('/<td>&nbsp;<\/td>/',$m[1][4])) {
+			list(,$prose) = explode('/>', $m[1][4]);
+			$prose = trim($prose);
+			$dto->setProseDescription($prose);
+		}
+		return $dto;
 	}
-	
-}
-
 
 ?>
