1098795149f3544cd606fc4c4486304c1a154ece
Harvey Quamen <hquamen@ualberta.ca>
2014-02-08 18:30:53 -0700

    Adding methods to calculate averages from Hourly page: wind speed, wind direction, humidity, pressure

--- AirportWeatherScraper.php
+++ AirportWeatherScraper.php
@@ -3,8 +3,16 @@
 class AirportWeatherScraper extends RealWeatherScraper {
 
 	protected $siteURL = 'http://climate.weather.gc.ca/climateData/dailydata_e.html?StationID=';
+	protected $hourlyURL = 'http://climate.weather.gc.ca/climateData/hourlydata_e.html?StationID=';
 	protected $stationID;
 	protected $cloudCoverURL = 'http://edmonton.weatherstats.ca/data/cloud_cover-2weeks.json';
+	
+	//	indexes for columns in the Environment Canada hourly page:
+	const SPEED_INDEX = 5;
+	const DIRECTION_INDEX = 4;
+	const PRESSURE_INDEX = 7;
+	const HUMIDITY_INDEX = 3;
+	const NOT_NUMERIC_DATA = '/[^-.0-9]/';
 
 	public function __construct($weathermanager = null) {
 		parent::__construct( $weathermanager );
@@ -13,7 +21,8 @@ class AirportWeatherScraper extends RealWeatherScraper {
 	}
 
 	public function scrape() {
-		if ( $this->scrapeEnvironmentCanada() and $this->scrapeCloudCover() ) {
+		if ( $this->scrapeEnvironmentCanada() and $this->scrapeCloudCover()
+			and $this->scrapeWind()) {
 			return count($this->weathercollection);
 		} else {
 			return false;
@@ -50,6 +59,108 @@ class AirportWeatherScraper extends RealWeatherScraper {
 		} else {
 			return false;
 		}
+	}
+	
+	public function scrapeWind() {
+		// average this from the hourly update
+		$this->buildHourlyURL();
+		$html = $this->cleanup(Utility_SecretAgent::getURL($this->hourlyURL));
+		if (empty($html)) {
+			Utility_Logger::log(__CLASS__ . 'cannot load Hourly page for station ID ' . $this->stationID);
+			return false;
+		}
+		$rows = $this->getHourlyRows($html);
+		$windSpeed = $this->getWindSpeed($rows);
+		$this->dto->setWindSpeed($windSpeed);
+		$this->dto->setWindSpeedUnit('km/h'); // yeah, hard-coded, but it's EnviroCan!
+		$windDirection = $this->getWindDirection($rows);	//	account for units of 10s
+		$this->dto->setWindDirection($windDirection);
+		$pressure = $this->getPressure($rows);
+		$this->dto->setPressure($pressure);
+		$humidity = $this->getHumidity($rows);
+		$this->dto->setHumidity($humidity);
+	}
+	
+	public function buildHourlyURL() {
+		//50149&timeframe=1&Year=2014&Month=1&cmdB1=Go&Day=31
+		$this->hourlyURL .= $this->stationID . '&timeframe=1&Year=' . $this->yesterday->getYear() . '&Month=' .
+			$this->yesterday->getMonth() . '&cmdB1=Go&Day=' . $this->yesterday->getDay();
+	}
+	
+	public function getHourlyRows($html) {
+		$clean_rows = array();
+		$table_regex = '/<tr>\s*<th>TIME\s*<\/th>(.+?)<\/table>/';
+		if (!preg_match($table_regex, $html, $table)) {
+			Utility_Logger::log(__CLASS__ . ': Hourly Table regex didn\'t match');
+			return null;
+		}
+		$row_regex = '/<tr>(.+?)<\/tr>/';
+		if (!preg_match_all($row_regex, $table[1], $rows)) {
+			Utility_Logger::log(__CLASS__ . ': Hourly Row regex didn\'t match');
+			return null;
+		}
+		$cell_regex = '/<td\s*>(.*?)<\/td>/';
+		foreach ($rows[1] as $row) {
+			if (!preg_match_all($cell_regex, $row, $cells)) {
+				Utility_Logger::log(__CLASS__ . ': Hourly Cell regex didn\'t match');
+				return null;
+			}
+			$clean_rows[] = $cells[1];
+		}
+		return $clean_rows;
+	}
+	
+	public function getWindSpeed($rows) {
+		$total = $count = 0;
+		foreach ($rows as $row) {
+			if (empty($row[self::SPEED_INDEX]) or 
+				preg_match(self::NOT_NUMERIC_DATA, $row[self::SPEED_INDEX])) continue;
+			$total += $row[self::SPEED_INDEX];
+			$count++;
+		}
+		return round($total / $count);
+	}
+	
+	public function getWindDirection($rows) {
+		//	Environment Canada measures this in units of 10 degrees;
+		//	Use trig -- convert to (x,y) points on the unit circle
+		//	and average, then find the arctangent of the averaged point.
+		$sum_x = $sum_y = $count = 0;
+		foreach ($rows as $row) {
+			if (empty($row[self::DIRECTION_INDEX]) or 
+				preg_match(self::NOT_NUMERIC_DATA, $row[self::DIRECTION_INDEX])) continue;
+			$dir = $row[self::DIRECTION_INDEX] * 10;
+			$sum_x += cos(deg2rad($dir));
+			$sum_y += sin(deg2rad($dir));
+			$count++;
+		}
+		$avg_x = $sum_x / $count;
+		$avg_y = $sum_y / $count;
+		$angle = rad2deg(atan($avg_y / $avg_x));
+		if ($angle < 0) $angle += 360;
+		return round($angle);
+	}
+	
+	public function getPressure($rows) {
+		$total = $count = 0;
+		foreach ($rows as $row) {
+			if (empty($row[self::PRESSURE_INDEX]) or 
+				preg_match(self::NOT_NUMERIC_DATA, $row[self::PRESSURE_INDEX])) continue;
+			$total += $row[self::PRESSURE_INDEX];
+			$count++;
+		}
+		return round(($total / $count), 2);
+	}
+	
+	public function getHumidity($rows) {
+		$total = $count = 0;
+		foreach ($rows as $row) {
+			if (empty($row[self::HUMIDITY_INDEX]) or 
+				preg_match(self::NOT_NUMERIC_DATA, $row[self::HUMIDITY_INDEX])) continue;
+			$total += $row[self::HUMIDITY_INDEX];
+			$count++;
+		}
+		return round($total / $count);
 	}
 	
 	public function validate($string) {
