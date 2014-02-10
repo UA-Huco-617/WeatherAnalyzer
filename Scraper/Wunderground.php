b7d4c4b9e34e232977f20790b12385168a5c160b
levdir <dmckechn@ualberta.ca>
2014-02-10 14:08:44 -0700

    Updated Wunderground

--- Scraper/Wunderground.php
+++ Scraper/Wunderground.php
@@ -1,6 +1,6 @@
 <?php
 
-class Scraper_Wunderground extends WeatherScraper{
+class Scraper_Wunderground extends Weather_WeatherScraper{
 
 	//	scrape() should do the following.
 	//	1. for each day's forecast:
@@ -15,17 +15,28 @@ class Scraper_Wunderground extends WeatherScraper{
 	//	6. while still more days, go to step 1
 	//	7. return count($this->weathercollection);
 	
-	protected $siteID = '10';
+	protected $siteID = 10;
 	protected $siteURL = 'http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=IALBERTA60&day=9&year=2014&month=2&graphspan=day';
 	
 	public function scrape(){
 
-		$wundergroundDTO = new Weather_WeatherDTO($this);
-
-		$regex = '/<td><span class="nobr"><span class="b">(.*?)<\/span>&nbsp;&deg;C<\/span><\/td>/';
+		$rows = $this->extractRowsFromHTML();
+		foreach($rows as $row){
+			$dto = $this->buildDTOFromRow($row);
+			$this->addToCollection($dto);
+		}//foreach
+		return count($this->weathercollection);
 
 	} //scrape()
 
+	public function extractRowsFromHTML($html){
+
+		$regex = '/<tbody>(<tr>(.+?)<\/tr>)+<\/tbody>/';
+		preg_match_all($regex, $html, $rows);
+		print_r($rows);
+		exit;
+
+	} //extractRowsFromHTML
 } //Scraper_Wunderground
 
 ?>
