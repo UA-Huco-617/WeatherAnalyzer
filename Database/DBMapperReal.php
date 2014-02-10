f9350fd90872f6780ee3abc2ed90f767b8c9ba37
Harvey Quamen <hquamen@ualberta.ca>
2014-02-09 23:57:45 -0700

    Adding rounding functions for more accurate data.

--- Database/DBMapperReal.php
+++ Database/DBMapperReal.php
@@ -17,19 +17,26 @@ class Database_DBMapperReal extends Database_DBMapper {
 		$raw = array_fill_keys(array_keys($this->columnType), 0);
 		$raw['site_id'] = $this->dto->getSiteID();
 		$raw['date'] = $this->dto->getForecastDate();	// will be yesterday
-		$raw['high'] = $this->dto->getHighTemp(); 
+		$raw['high'] = $this->dto->getHighTemp();
+			if (isset($raw['high'])) $raw['high'] = round($raw['high'], 1);
 		$raw['high_unit'] = $this->dto->getTempUnit();
 		$raw['low'] = $this->dto->getLowTemp();
+			if (isset($raw['low'])) $raw['low'] = round($raw['low'], 1);
 		$raw['low_unit'] = $this->dto->getTempUnit();
 		$raw['precip'] = $this->dto->getPrecipitation();
+			if (isset($raw['precip'])) $raw['precip'] = round($raw['precip'], 1);
 		$raw['precip_unit'] = $this->dto->getPrecipitationUnit();
 		$raw['cloud_cover'] = $this->dto->getCloudCover();
+			if (isset($raw['cloud_cover'])) $raw['cloud_cover'] = round($raw['cloud_cover']);
 		$raw['wind_speed'] = $this->dto->getWindSpeed();
+			if (isset($raw['wind_speed'])) $raw['wind_speed'] = round($raw['wind_speed']);
 		$raw['wind_unit'] = $this->dto->getWindSpeedUnit();
 		$raw['wind_direction'] = $this->dto->getWindDirection();
+			if (isset($raw['wind_direction'])) $raw['wind_direction'] = round($raw['wind_direction']);
 		$raw['pressure'] = $this->dto->getPressure();
+			if (isset($raw['pressure'])) $raw['pressure'] = round($raw['pressure'], 2);
 		$raw['humidity'] = $this->dto->getHumidity();
-		//$raw['prose_description'] = $this->dto->getProseDescription();
+			if (isset($raw['humidity'])) $raw['humidity'] = round($raw['humidity']);
 		return $raw;
 	}
 
