1329d12888e96b8f6fafac34cf2cbc2d0167437c
Harvey Quamen <hquamen@ualberta.ca>
2014-02-12 10:13:13 -0700

    Adding a line to register the autoload function. Helps with running PHPUnit tests.

--- autoload.php
+++ autoload.php
@@ -1,5 +1,8 @@
 <?php
 
+//	Register this autoload function so that things like PHPUnit can see it.
+spl_autoload_register('__autoload');
+
 function __autoload($class) {
 	$class = str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
 	require_once $class;
