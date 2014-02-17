4ed2e8ed885ffcb50f5dda2b362afdddc80b6fd3
Harvey Quamen <hquamen@ualberta.ca>
2014-02-17 14:33:42 -0700

    Debug and modify regular expressions. Looks good to go.

--- Scraper/EnvironmentCanada.php
+++ Scraper/EnvironmentCanada.php
@@ -1,191 +1,95 @@
 <?php
 class Scraper_EnvironmentCanada extends Weather_WeatherScraper{
 
-	protected $siteID = '11';				
-	protected $siteURL = 'http://weather.gc.ca/city/pages/ab-50_metric_e.html';			
+	protected $siteID = 11;				
+	protected $siteURL = 'http://weather.gc.ca/city/pages/ab-50_metric_e.html';	
+	protected $detailedForecast;	//	contains longer prose, including wind info		
 	
 	public function scrape(){
-		$rows= $this->extractRowsFromHTML($this->html);
-		foreach ($rows as $row) {
-			$dto = $this->buildDTOFromRow($row);
+		$divs = $this->extractDIVsFromHTML($this->html);
+		$this->detailedForecast = $this->extractDetailedForecast($this->html);
+		foreach ($divs as $div) {
+			$dto = $this->buildDTOFromDIV($div);
 			$this->addToCollection($dto);
 		}
 		return count($this->weathercollection);
-
 	}
 
-	public function extractRowsFromHTML($html){
-		$html = str_replace("\n", "", $html);
-		$regexhtml = '/<h4>(.+?)<\/div>/';
-		preg_match_all($regexhtml, $html, $rows);
-		$rows = $rows[0];
-		return $rows;
+	public function extractDIVsFromHTML($html) {
+		$div_regex = '/<div class="fperiod">(.+?)<\/div>/';
+		preg_match_all($div_regex, $html, $divs);
+		return $divs[1];
 	}
 
+	public function extractDetailedForecast($html) {
+		$regex = '/<div class="fdetails">(.+?)<\/div>/';
+		preg_match($regex, $html, $details);
+		return $details[1];
+	}
 
-
-	
-	public function buildDTOFromRow($row){
+	public function buildDTOFromDIV($div){
 		$dto = new Weather_WeatherDTO($this);
-		
-		$html = file_get_contents('http://weather.gc.ca/city/pages/ab-50_metric_e.html');
-		$html = str_replace("\n", "", $html);
-		$regex = '/"mainContent">(.+?)"wb-invisible">D/';
-		preg_match_all($regex, $html, $cells);
-		$cells = $cells[0];
 
+		//----------------------------------------------------------------------------
+		//	DATE
+		$date_regex = '/<abbr title="(\w+)">(?:\w+)<\/abbr>\s*<br><span class="fdate">(\d+)&nbsp;<abbr title="(\w+)">/';
+		preg_match($date_regex, $div, $date);
+		//	will look like => Monday 17 February
+		//	ignore the year; strtotime() will assume current year
+		$date_string = $date[1] . ' ' . $date[2] . ' ' . $date[3];
+		$dto->setForecastDate($date_string);
 
-		//DATE
+		//----------------------------------------------------------------------------
+		//	Prose Description
+		//	let's get this from the detailed forecast rather than from the img's @alt
+		$date_header = $date[2] . '&nbsp;' . $date[3];
+		$prose_regex = "/{$date_header}<\/span><\/dt>\s*<dd>(.+?)<\/dd>/";
+		preg_match($prose_regex, $this->detailedForecast, $prose);
+		$dto->setProseDescription(trim($prose[1]));
 
-		$fregex = '/"fdate">(\d+)&nbsp;<abbr title="(\w+)">/';
-		preg_match_all($fregex, $cells[0], $date);
-		$x= 0;
-		foreach ($date[1] as $value) {
-			
-			$date[1][$x] = $date[1][$x] . " " . $date[2][$x] . " 2014";
-			$date[1][$x] = date('d/m/Y', strtotime($date[1][$x]));
-			$x++;
+		//----------------------------------------------------------------------------
+		//	Wind
+		//	don't take this from the current conditions at the top of the page; rather,
+		//	let's use the appropriate prose sentence in the detailed forecast:
+		//	"A mix of sun and cloud. Wind becoming west 20 km/h gusting to 40 this afternoon. High plus 2."
+
+		$wind_regex = '/Wind becoming (\w+) (\d+) (.+?)[. ]/';
+		//	sometimes that regex won't match, so don't set Wind data unless we get a hit.
+		if (preg_match($wind_regex, $prose[1], $windData)) {
+			$direction = Utility_WindDirection::getDegrees($windData[1]);
+			$dto->setWindDirection($direction);
+			$dto->setWindSpeed($windData[2]);
+			$dto->setWindSpeedUnit($windData[3]);
 		}
-		$date = $date[1];
-		$dto->setForecastDate($date);
 
-
-		$dto->setDate($date[0]);
-
-
-
-
-		//Wind Rows
-
-		$windRow_regex = '/"longContent">(.+?)<\/dd>/';
-		preg_match_all($windRow_regex, $cells[0], $windRows);
-		$windRows = $windRows[0][0];
-
-
-
-		//Wind Speed Unit
-
-		$windSpeedUnit_regex = '/km\/h/';
-		preg_match_all($windSpeedUnit_regex, $cells[0], $windSpeedUnit);
-		$windSpeedUnit = $windSpeedUnit[0];
-		$dto->setWindSpeedUnit($windSpeedUnit);
-
-
-
-		//Wind Direction and Speed
-
-		$directionspeed_regex = '/(\w+)<\/abbr>\s*(\d+)\s*<abbr title/';
-		preg_match_all($directionspeed_regex, $windRows, $directionspeed);
-		$windSpeed = $directionspeed[2];
-		$direction = $directionspeed[1];
-		$dto->setWindSpeed($windSpeed);
-		$dto->setWindDirection($direction);
-
-
-
-		//Description
-
-		$description_regex = '/.gif" alt="(.+?)"/';
-		preg_match_all($description_regex, $cells[0], $description);
-		$description = $description[1];
-		unset($description[1]);
-		$description = array_values($description);
-		$dto->setProseDescription($description);
-
-
-
-
+		//----------------------------------------------------------------------------
 		//Chance of Precipitation
-
-		$COP_regex = '/"pop"(.+?)<\/li> /';
-		preg_match_all($COP_regex, $cells[0], $COP);
-		$COP = $COP[1];
-		$x = 0;
-		foreach ($COP as $value) {
-			if (strcspn($COP[$x], '0123456789') != strlen($COP[$x])){
-				preg_match_all('/[0-9][0-9]?%/', $COP[$x], $number);
-				$COP[$x] = $number[0][0];
-				$x++;}
-			else{
-				$COP[$x] = null;
-				$x++;
-			}
+		//	if it's non-existent, this regex won't match. If there's a %, it's this:
+		//	<li class="pop" title="Chance of Precipitation"> 60%</li>
+		$COP_regex = '/title="Chance of Precipitation">\s*(\d+)%\s*<\/li>/';
+		if (preg_match($COP_regex, $div, $cop)) {
+			$dto->setChanceOfPrecip($cop[1]);
 		}
-		$dto->setChanceOfPrecip($COP);
-
-
-
-		//High - High not provided for all dates
-
-		$high_regex = '/"high"(.+?)<\/li>/';
-		preg_match_all($high_regex, $cells[0], $high);
-		$high = $high[1];
-		$x=0;
-		foreach ($high as $value) {
-			if (strcspn($high[$x], '0123456789') != strlen($high[$x])){
-				preg_match_all('/-?[0-9][0-9]?[0-9]?/',$high[$x] , $matches);
-				$high[$x] = $matches[0][0];
-			$x++;}
-			else{
-				$high[$x] = null;
-				$x++;
-			}
-		}	
-		$dto->setHighTemp($high);
-
 		
-
-		//Low - Low not provided for all dates
-
-		$low_regex = '/"low"(.+?)<\/li>/';
-		preg_match_all($low_regex, $cells[0], $low);
-		$low = $low[1];
-		$x = 0;
-		foreach ($low as $value) {
-			if ($low[$x] == '>&nbsp'){
-				$low[$x] = null;
-				$x = $x++;
-			}
-			if (strcspn($low[$x], '0123456789') != strlen($low[$x])){
-				preg_match_all('/-?[0-9][0-9]?[0-9]?/',$low[$x] , $matches);
-				$low[$x] = $matches[0][0];
-			$x++;}
-			else{
-				$low[$x] = null;
-				$x++;
-			}
+		//----------------------------------------------------------------------------
+		//High - High not provided for all dates
+		$high_regex = '/title="High">([-.0-9]+)&deg;<abbr title="(?:\w+)">(\w+)<\/abbr>/';
+		if (preg_match($high_regex, $div, $high)) {
+			print_r($high);
+			$dto->setHighTemp($high[1]);
+			echo "Retrieving: " . $dto->getHighTemp() . "\n";
+			$dto->setTempUnit($high[2]);
 		}
-			$dto->setLowTemp($low);
-			
+		
+		//----------------------------------------------------------------------------
+		//	Low - low not provided for all dates
+		$low_regex = '/title="Low">([-.0-9]+)&deg;<abbr title="(?:\w+)">(\w+)<\/abbr>/';
+		if (preg_match($low_regex, $div, $low)) {
+			$dto->setLowTemp($low[1]);
+			//	assume low tmp unit == high temp unit
+		}
 
-
-			//Pressure
-
-			$pressure_regex='/Pressure.+?([.0-9]+)&nbsp;/';
-			preg_match_all($pressure_regex, $cells[0], $pressure);
-			$pressure = $pressure[1];
-			$dto->setPressure($pressure);
-
-
-			//Pressureunit
-
-			$pressureunit_regex='/"kilopascals">(kPa)<\/abbr>/';
-			preg_match_all($pressureunit_regex, $cells[0], $pressureunit);
-			$pressureunit = $pressureunit[1];
-			$dto->setPressureUnit($pressureunit);
-
-
-
-
-			//Humidity
-			$humidity_regex = '/Humidity:<\/dt>.+?([.0-9]+%)<\/dd>/';
-			preg_match_all($humidity_regex, $cells[0], $humidity);
-			$humidity = $humidity[1];
-			$dto->setHumidity($humidity);
-
-			return $dto;
-
-
+		return $dto;
 	}
 
 
