f9528fbef174a16162ea2114bc43962f49c559a8
Harvey Quamen <hquamen@ualberta.ca>
2014-02-17 14:34:20 -0700

    Extending Brett's class to scrape forecast from International Airport. ID 12.

---
+++ Scraper/EnvironmentCanadaInternational.php
@@ -0,0 +1,8 @@
+<?php
+
+class Scraper_EnvironmentCanadaInternational extends Scraper_EnvironmentCanada {
+
+	protected $siteID = 12;				
+	protected $siteURL = 'http://weather.gc.ca/city/pages/ab-71_metric_e.html';
+}
+?>
