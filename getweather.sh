a7af3667e70c1afa55b675536ea57e10d806a55e
Harvey Quamen <hquamen@ualberta.ca>
2014-02-17 21:47:11 -0700

    Setting correct path to driver

--- getweather.sh
+++ getweather.sh
@@ -3,4 +3,4 @@ RANDOM=$$
 # 900 secs == 15 mins
 wait=$(expr $RANDOM % 900)
 sleep $wait
-/usr/local/bin/php /home/hquamen/weather/driver.php >> /home/hquamen/web-docs/weather/log.txt 2>&1
+/usr/local/bin/php /home/hquamen/weatheranalyzer/driver.php >> /home/hquamen/web-docs/weather/log.txt 2>&1
