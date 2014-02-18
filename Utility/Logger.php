0cb58d1dd2329d2c02eb40cf4188000009417b5b
Harvey Quamen <hquamen@ualberta.ca>
2014-02-17 21:54:28 -0700

    Adding seconds to timestamp for Logger.

--- Utility/Logger.php
+++ Utility/Logger.php
@@ -6,7 +6,7 @@ class Utility_Logger {
 	
 	public static function log($message = '') {
 		date_default_timezone_set('America/Edmonton');
-		$message = date('D j M Y g:i a') . ' -- ' . $message . "\n";
+		$message = date('D j M Y g:i:s a') . ' -- ' . $message . "\n";
 		@file_put_contents(self::LOGFILE, $message, FILE_APPEND);
 	}
 }
