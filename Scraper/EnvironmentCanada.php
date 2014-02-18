14efed9654e43ef84ce3b9362b23438442ebc3b6
Harvey Quamen <hquamen@ualberta.ca>
2014-02-17 21:24:43 -0700

    Fixing two regular expressions that broke already: date and wind.

--- Scraper/EnvironmentCanada.php
+++ Scraper/EnvironmentCanada.php
@@ -32,17 +32,24 @@ class Scraper_EnvironmentCanada extends Weather_WeatherScraper{
 
 		//----------------------------------------------------------------------------
 		//	DATE
-		$date_regex = '/<abbr title="(\w+)">(?:\w+)<\/abbr>\s*<br><span class="fdate">(\d+)&nbsp;<abbr title="(\w+)">/';
-		preg_match($date_regex, $div, $date);
-		//	will look like => Monday 17 February
-		//	ignore the year; strtotime() will assume current year
-		$date_string = $date[1] . ' ' . $date[2] . ' ' . $date[3];
-		$dto->setForecastDate($date_string);
+		//	looks like:
+		//	<span class="fdate">17&nbsp;<abbr title="February">Feb</abbr>
+		$date_regex = '/<span class="fdate">(\d+)&nbsp;<abbr title="(\w+)">/';
+		if (preg_match($date_regex, $div, $date)) {
+			//	will look like => Monday 17 February
+			//	ignore the year; strtotime() will assume current year
+			$date_string = $date[1] . ' ' . $date[2];
+			$dto->setForecastDate($date_string);
+		} else {
+			Utility_Logger::log(__METHOD__ . ' could not parse a proper date. HTML changed? Returning without a DTO.');
+			return null;
+		}
+		
 
 		//----------------------------------------------------------------------------
 		//	Prose Description
 		//	let's get this from the detailed forecast rather than from the img's @alt
-		$date_header = $date[2] . '&nbsp;' . $date[3];
+		$date_header = $date[1] . '&nbsp;' . $date[2];
 		$prose_regex = "/{$date_header}<\/span><\/dt>\s*<dd>(.+?)<\/dd>/";
 		preg_match($prose_regex, $this->detailedForecast, $prose);
 		$dto->setProseDescription(trim($prose[1]));
@@ -53,7 +60,7 @@ class Scraper_EnvironmentCanada extends Weather_WeatherScraper{
 		//	let's use the appropriate prose sentence in the detailed forecast:
 		//	"A mix of sun and cloud. Wind becoming west 20 km/h gusting to 40 this afternoon. High plus 2."
 
-		$wind_regex = '/Wind becoming (\w+) (\d+) (.+?)[. ]/';
+		$wind_regex = '/Wind (?:becoming )?(\w+) (\d+) (.+?)[. ]/';
 		//	sometimes that regex won't match, so don't set Wind data unless we get a hit.
 		if (preg_match($wind_regex, $prose[1], $windData)) {
 			$direction = Utility_WindDirection::getDegrees($windData[1]);
@@ -66,6 +73,8 @@ class Scraper_EnvironmentCanada extends Weather_WeatherScraper{
 		//Chance of Precipitation
 		//	if it's non-existent, this regex won't match. If there's a %, it's this:
 		//	<li class="pop" title="Chance of Precipitation"> 60%</li>
+		//	Note: sometimes this appears as prose in the Detailed Forecast, but
+		//	not in the DIVs at the top. Should this part be smarter?
 		$COP_regex = '/title="Chance of Precipitation">\s*(\d+)%\s*<\/li>/';
 		if (preg_match($COP_regex, $div, $cop)) {
 			$dto->setChanceOfPrecip($cop[1]);
@@ -75,10 +84,7 @@ class Scraper_EnvironmentCanada extends Weather_WeatherScraper{
 		//High - High not provided for all dates
 		$high_regex = '/title="High">([-.0-9]+)&deg;<abbr title="(?:\w+)">(\w+)<\/abbr>/';
 		if (preg_match($high_regex, $div, $high)) {
-			print_r($high);
 			$dto->setHighTemp($high[1]);
-			echo "Retrieving: " . $dto->getHighTemp() . "\n";
-			$dto->setTempUnit($high[2]);
 		}
 		
 		//----------------------------------------------------------------------------
