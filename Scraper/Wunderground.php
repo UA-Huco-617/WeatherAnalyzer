62dd9a874f3819c72e14d6262b435aaa29b69ec8
levdir <dmckechn@ualberta.ca>
2014-03-05 16:31:11 -0700

    Added update Wunderground.php

--- Scraper/Wunderground.php
+++ Scraper/Wunderground.php
@@ -1,6 +1,6 @@
 <?php
 
-class Scraper_Wunderground extends WeatherScraper{
+class Scraper_Wunderground extends Weather_WeatherScraper{
 
 	//	scrape() should do the following.
 	//	1. for each day's forecast:
@@ -15,17 +15,51 @@ class Scraper_Wunderground extends WeatherScraper{
 	//	6. while still more days, go to step 1
 	//	7. return count($this->weathercollection);
 	
-	protected $siteID = '10';
-	protected $siteURL = 'http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=IALBERTA60&day=9&year=2014&month=2&graphspan=day';
+	protected $siteID = 10;
+	protected $siteURL = 'http://www.wunderground.com/global/stations/71123.html?MR=1';
 	
 	public function scrape(){
 
-		$wundergroundDTO = new Weather_WeatherDTO($this);
+		$arrayForecast = $this->getProseForecast();
+		foreach ($arrayForecast as $forecastDay => $forecastProse) {
+			$weatherdto = new Weather_WeatherDTO($this);
+			$weatherdto->setForecastDate($forecastDay);
+			$weatherdto->setProseDescription($forecastProse);
+			$this->addToCollection($weatherdto);
+		} // foreach
 
-		$regex = '/<td><span class="nobr"><span class="b">(.*?)<\/span>&nbsp;&deg;C<\/span><\/td>/';
+		return count($this->weathercollection);
 
-	} //scrape()
+	} // scrape()
 
-} //Scraper_Wunderground
+	public function getProseForecast() {
+		$html = file_get_contents("http://www.wunderground.com/global/stations/71123.html?MR=1");
+		$html = str_replace("\n", '', $html);
+		$div_regex = '/<div class="p5">(.+?)<\/table>\s*<\/div>/';
+		$arrayForecast = array();
+		preg_match_all($div_regex, $html, $divs);
+		foreach ($divs[1] as $div) {
+			$currentDay = $this->getDay($div);
+			$currentProseForecast = $this->getProse($div);
+			$arrayForecast[$currentDay] = $currentProseForecast;			
+		} // foreach
+		return $arrayForecast;
+	}
+
+	protected function getDay($divDay){
+		$dayRegex = '/<div class="b">(.+?)<\/div>/';
+		preg_match($dayRegex, $divDay, $day);
+		echo $day[1]."\n";
+		return $currentDay = $day[1];
+	} // getDay
+
+	protected function getProse($divProse){
+		$proseRegex = '/<td class="vaT full">(.+?)<\/td>/';
+		preg_match($proseRegex, $divProse, $prose);
+		echo $prose[1]."\n\n";
+		return $currentProseForecast = $prose[1];
+	} // getProse
+
+} // Scraper_Wunderground
 
 ?>
