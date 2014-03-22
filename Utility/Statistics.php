1eafdc5549ea8e2514db60173c8133c89eb06cce
Harvey Quamen <hquamen@ualberta.ca>
2014-03-22 13:17:03 -0600

    Preventing division by zero in Statistics class.

--- Utility/Statistics.php
+++ Utility/Statistics.php
@@ -3,12 +3,14 @@
 class Utility_Statistics {
 	
 	public static function getAverage($array) {
+		if (count($array) == 0) return 0;
 		return array_sum($array) / count($array);
 	}
 
 	public static function getLinearRegressionCoefficient($array) {
 		//	treats indexes as X, values as Y; for weather, typically
 		//	the x-values are hours of the day (0-23).
+		if (count($array) == 0) return 0;
 		$mult = array();
 		$avg_x = array_sum(array_keys($array)) / count($array);
 		$avg_y = array_sum($array) / count($array);
